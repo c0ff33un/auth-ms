7b02209eaef03e5271be6595dedffbc55a712a80
Edwin M <ermahechap@unal.edu.co>
2019-10-08 20:12:36 -0500

    Minor change

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,11 @@ module AuthMs
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    
+    ## User for UUID
+    #config.generators do |g|
+    #  g.orm :active_record, primary_key_type: :uuid
+    #end
+
   end
 end
